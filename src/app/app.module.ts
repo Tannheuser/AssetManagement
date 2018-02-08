1a124561d2a2f7fa57c531c3025280739f4dd053
Aleksey Ryabinin <aleksey.ryabinin@gmail.com>
2018-02-08 17:21:52 +0300

    Fix http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -24,6 +25,7 @@ import { RootComponent } from './root.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     EffectsModule.forRoot([]),
     RoutingModule,
     SharedModule,
